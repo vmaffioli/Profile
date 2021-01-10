ad50a1aa92df5f08503f8c954f80a1523af4ff12
vmaffioli <vinimaffioli@gmail.com>
2021-01-09 23:37:57 -0300

    fixed meta tags

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import MetaTags from 'react-meta-tags';
 
 import './reset.css';
 import App from './pages/Home/App';
 import Work from './pages/Work';
 
-import Thumb from './assets/img/thumb.png';
-
-
-
 
 ReactDOM.render(
   
   <React.StrictMode>
-    
-    <MetaTags>
-      
-        <title>Vinícius Maffioli</title>
-        <meta name="description" content="My Web Profile made with React.js" />
-        <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
-        <meta name="author" content="Vinícius Maffioli" />
-        <meta property="og:image" content={Thumb} />
-        
-    </MetaTags>
-
     <BrowserRouter>
-
-
+    
       <Switch>
           
           <Route path="/" component={App} exact />
