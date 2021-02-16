4a3a27c3bf0811183efb95a8a2f2ad1d548ec85a
vmaffioli <vinimaffioli@gmail.com>
2021-02-15 23:08:23 -0300

    added setState with btn on work page for chat and chat info

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -11,20 +11,13 @@ function Skills(props) {
     <div className="skills-div ">
 
       <div className="skills">
-        <h2 id="h2-skills">Minhas Skills / Autonomia</h2>
-        <ul>
-          <li><span className="html5"></span><em>HTML</em></li>
-          <li><span className="css3"></span><em>CSS</em></li>
-          <li><span className="js"></span><em>Javascript</em></li>
-          <li><span className="reactjs"></span><em>React.js</em></li>
-          <li><span className="nodejs"></span><em>Node.js</em></li>
-          <li><span className="java"></span><em>JAVA</em></li>
-          <li><span className="sql"></span><em>SQL (mySQL)</em></li>
-
-
-        </ul>
-
+        <h2 id="h2-skills">Chatbot</h2>
+        <p>Utilizando o pacote react-chatbot-kit, criei um chatbot simples para responder perguntas frequentes em entrevistas e recrutamentos</p>
+        <p>DICA<br/>Para conferir as respostas disponíveis pergunte:<br/>'O que você sabe responder?'</p>
+        <p>Vamos começar?</p>
       </div>
+
+      
       
     </div>
 
--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
 import PrevPage from '../../components/PrevPage';
 import Chatbot from 'react-chatbot-kit';
 import Exp from '../../components/Exp';
+import Skills from '../../components/Skills';
+
 
 import ActionProvider from "../../components/Chatbot/ActionProvider";
 import MessageParser from "../../components/Chatbot/MessageParser";
@@ -14,28 +17,59 @@ import config from "../../components/Chatbot/config";
 
 
 function Work() {
+  const buttonIn = <button className="btn-chat_out" onClick={element => setWorkHistory({
+    element: sendChatInfo
+  })}>VOLTAR</button>;
+
+  const buttonOut = <button className="btn-chat" onClick={element => setWorkHistory({
+    element: sendChat
+
+  })}>Falar com Vinibot</button>;
+
+  const sendChat = <div className="chat-container">
+    <Chatbot
+      config={config}
+      actionProvider={ActionProvider}
+      messageParser={MessageParser}
+      headerText="VINIBOT - Informações extras"
+    />
+
+    {buttonIn}
+  </div>;
+
+  const sendChatInfo = <div>
+    <Skills />
+    {buttonOut}
+
+  </div>;
+
+  const [workHistory, setWorkHistory] = useState({
+    element: sendChatInfo
+
+  });
+
   return (
     <div className="work">
 
-        <div className="style-bars" id="top-bar_w"/>
-        <div className="style-bars" id="bottom-bar_w"/>
+      <div className="style-bars" id="top-bar_w" />
+      <div className="style-bars" id="bottom-bar_w" />
 
       <Link to="/">
         <PrevPage />
       </Link>
 
-      <div className="chat-container">
-        <Chatbot 
-            config={config}
-            actionProvider={ActionProvider}
-            messageParser={MessageParser}
-            headerText= "VINIBOT - Informações extras"
-        />
+
+
+
+
+      <div className="content-div">
+        <div>{workHistory.element}</div>
+
       </div>
 
-      
 
-      <Exp  />
+
+      <Exp />
 
 
     </div>
